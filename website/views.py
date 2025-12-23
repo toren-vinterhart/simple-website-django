f5c5dd1e3e0065e9df20d3e2a6b2f0a9d5fae7c7
toren-vinterhart <torenvinterhart@gmail.com>
2025-12-23 19:53:10 +0330

    Add some html files to website app

--- website/views.py
+++ website/views.py
@@ -4,11 +4,11 @@ from django.http import HttpResponse, JsonResponse
 # Create your views here.
 
 def index_view(request):
-    return HttpResponse('<h1>Home Page</h1>')
+    return render(request, 'website/index.html')
 
 def about_view(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request, 'website/about.html')
 
 def contact_view(request):
-    return HttpResponse('<h1>Contact Page</h1>')
+    return render(request, 'website/contact.html')
 
