191f618b1df93900baf6a941b679c433d9b809db
toren-vinterhart <torenvinterhart@gmail.com>
2025-12-23 18:18:08 +0330

    Create website app

---
+++ website/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+# Create your views here.
+
+def index_view(request):
+    return HttpResponse('<h1>Home Page</h1>')
+
+def about_view(request):
+    return HttpResponse('<h1>About Page</h1>')
+
+def contact_view(request):
+    return HttpResponse('<h1>Contact Page</h1>')
+
