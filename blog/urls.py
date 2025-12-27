51ddba001b5e8c8b2d97137ce9dc3e93dfbc8cdd
toren-vinterhart <torenvinterhart@gmail.com>
2025-12-27 20:00:49 +0330

    Create blog app

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.blog_view, name='index'),
+    path('single/', views.blog_single, name='single'),
+]
