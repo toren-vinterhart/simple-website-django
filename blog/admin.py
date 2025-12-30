30c43369bc1f244d32e4b5d413a023af2338b439
toren-vinterhart <torenvinterhart@gmail.com>
2025-12-30 22:21:03 +0330

    Register Post model into admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from blog.models import Post
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    # fields = ('title',)
+    list_display = ('title', 'counted_views', 'status', 'published_date', 'created_date')
+    list_filter = ('status',)
+    ordering = ['-created_date']
+    search_fields = ['title', 'content']
+    list_editable = ['status']
+
+# admin.site.register(Post, PostAdmin)
