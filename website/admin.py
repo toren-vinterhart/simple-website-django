4d8dbd893cb9fd7885b9bd15ac26955146e9633c
toren-vinterhart <torenvinterhart@gmail.com>
2025-12-30 23:00:00 +0330

    Register Contact model into admin

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from website.models import Contact
 
 # Register your models here.
+
+@admin.register(Contact)
+class ContactModel(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    list_display = ('name', 'email', 'created_date')
+    list_filter = ('email',)
+    search_fields = ('name', 'message')
--- website/models.py
+++ website/models.py
@@ -9,4 +9,10 @@ class Contact(models.Model):
     message = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['created_date']
     
