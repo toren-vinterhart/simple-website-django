6bbad57191a3bd6e123e34cf1be570ea8723231c
toren-vinterhart <torenvinterhart@gmail.com>
2026-01-05 17:27:45 +0330

    Use trancatechars template-tag for summurizing post content in blog-home

--- blog/models.py
+++ blog/models.py
@@ -13,19 +13,25 @@ class Category(models.Model):
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
-    content = models.TextField()
     category = models.ManyToManyField(Category)
+
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
     # tag
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
+    
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date =  models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return "{} - {}".format(self.title, self.id)
+    
+    # @property
+    # def snippets(self):
+    #     return self.content[:100] + '...'
     
     class Meta:
         ordering = ['-created_date']
