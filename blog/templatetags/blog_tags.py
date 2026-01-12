5ded17457c53ed5b4a270d6e75b63cb48195a950
toren-vinterhart <torenvinterhart@gmail.com>
2026-01-12 21:47:15 +0330

    Make showing post categories dynamic with inclusion_tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Category
 
 register = template.Library()
 
@@ -30,3 +30,17 @@ def testposts():
 def latestposts(arg=3):
     posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
     return {'posts': posts}
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories():
+    # categories = Category.objects.all()
+    # return {'categories': categories}
+    
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+
+    return {'categories': cat_dict}
