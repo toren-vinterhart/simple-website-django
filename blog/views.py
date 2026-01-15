0e24a6cf0d049b28950cc3a043388a933c70750b
toren-vinterhart <torenvinterhart@gmail.com>
2026-01-15 18:13:55 +0330

    Concatinate blog_category view into blog_view

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,10 @@ from blog.models import Post, Category
 
 # Create your views here.
 
-def blog_view(request):
+def blog_view(request, cat_name=None):
     posts = Post.objects.filter(status=1)
+    if cat_name:
+        posts = posts.filter(category__name__iexact=cat_name)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
@@ -13,11 +15,11 @@ def blog_single(request, pid):
     context = {'post': post}
     return render(request, 'blog/blog-single.html', context)
 
-def blog_category(request, cat_name):
-    posts = Post.objects.filter(status=1)
-    posts = posts.filter(category__name__iexact=cat_name)
-    context = {'posts': posts}
-    return render(request, 'blog/blog-home.html', context)
+# def blog_category(request, cat_name):
+#     posts = Post.objects.filter(status=1)
+#     posts = posts.filter(category__name__iexact=cat_name)
+#     context = {'posts': posts}
+#     return render(request, 'blog/blog-home.html', context)
 
 def test(request):
     return render(request, 'test.html')
