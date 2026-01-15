0ae81a77348015c2c012f3035c5141c7ba1013a0
toren-vinterhart <torenvinterhart@gmail.com>
2026-01-15 20:17:16 +0330

    Create blog_search view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from blog.models import Post, Category
+from blog.models import Post
 
 # Create your views here.
 
@@ -16,6 +16,14 @@ def blog_single(request, pid):
     post = get_object_or_404(Post, id=pid, status=1)
     context = {'post': post}
     return render(request, 'blog/blog-single.html', context)
+
+def blog_search(request):
+    posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        if s := request.GET.get('s'):
+            posts = posts.filter(content__contains=s)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
 
 # def blog_category(request, cat_name):
 #     posts = Post.objects.filter(status=1)
