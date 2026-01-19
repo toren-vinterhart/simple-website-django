7d7c2f9ebb8ac0696d846401bdf3e2405adef88d
toren-vinterhart <torenvinterhart@gmail.com>
2026-01-19 22:01:48 +0330

    Create a simple-form for testing forms.Form

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from website.models import Contact
+from website.forms import NameForm
 
 # Create your views here.
 
@@ -15,15 +16,34 @@ def contact_view(request):
 
 def test_view(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        subject = request.POST.get('subject')
-        message = request.POST.get('message')
-        c = Contact(name=name, email=email, subject=subject, message=message)
-        # c.name = name
-        # c.email = email
-        # c.subject = subject
-        # c.message = message
-        c.save()
+        form = NameForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            print(name, email, subject, message)
+            c = Contact(name=name, email=email, subject=subject, message=message)
+            c.save()
+            return HttpResponse('done!')
+        else:
+            return HttpResponse('not valid')
 
-    return render(request, 'test.html')
+    form = NameForm()
+    context = {'form': form}
+    return render(request, 'test.html', context)
+
+# def test_view(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         email = request.POST.get('email')
+#         subject = request.POST.get('subject')
+#         message = request.POST.get('message')
+#         c = Contact(name=name, email=email, subject=subject, message=message)
+#         # c.name = name
+#         # c.email = email
+#         # c.subject = subject
+#         # c.message = message
+#         c.save()
+
+#     return render(request, 'test.html')
