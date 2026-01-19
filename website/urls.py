64d88f5f7bf8f83c1458026229cd20777f50a298
toren-vinterhart <torenvinterhart@gmail.com>
2026-01-19 20:02:01 +0330

    Move simple-form view function from blog to website app and do related changes

--- website/urls.py
+++ website/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index_view, name='index'),
     path('about/', views.about_view, name='about'),
     path('contact/', views.contact_view, name='contact'),
+    path('test/', views.test_view, name='test'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from website.models import Contact
 
 # Create your views here.
 
@@ -12,3 +13,17 @@ def about_view(request):
 def contact_view(request):
     return render(request, 'website/contact.html')
 
+def test_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        c = Contact(name=name, email=email, subject=subject, message=message)
+        # c.name = name
+        # c.email = email
+        # c.subject = subject
+        # c.message = message
+        c.save()
+
+    return render(request, 'test.html')
