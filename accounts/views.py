481eeff03d54ef5d6c025ec3ef538ee4f16cbd80
toren-vinterhart <torenvinterhart@gmail.com>
2026-02-02 20:29:05 +0330

    Implement login page

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 def login_view(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # return redirect('/')
+            return redirect('website:index')
+        else:
+            pass
+
+    context = {}
+    return render(request, 'accounts/login.html', context)
 
 def logout_view(request):
     pass
